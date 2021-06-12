5d32db778db1bf7bf883da964b225ddb73a2505d
折影轻梦 <nexmoe@qq.com>
2021-06-12 12:01:22 +0800

    npm 主题

--- themes/hexo-theme-yet-the-books/source/js/core.js
+++
@@ -1,6 +0,0 @@
-function search() {
-	window.open(
-        document.getElementById("search_form").getAttribute("action_e") + " " + document.getElementById("search_value").value
-	);
-	return false;
-}
